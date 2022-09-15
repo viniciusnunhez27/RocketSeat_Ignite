34ee31642ef70c2f24aa40cc729d4870469e9dc8
vinicius <v.nunhez27@gmail.com>
2022-09-15 01:09:49 -0300

    styles: Summary

--- dtmoney/src/components/Summary/index.tsx
+++ dtmoney/src/components/Summary/index.tsx
@@ -1,9 +1,40 @@
+import { Container } from "./sytles";
+import inconmeImg from  "../../assets/income.svg"
+import outcomeImg from  "../../assets/outcome.svg"
+import totalImg from  "../../assets/total.svg"
 
 
 
 export function Summary () {
    return (
-   <h1>Aham</h1>
-   )
+    <Container>
+        <div>
+           <header>
+            <p>Entradas</p>
+            <img src={inconmeImg} alt="icone-de-entrada" />
+            </header>
+            <strong>R$1000</strong>
+        </div>
 
+        <div>
+           <header>
+            <p>Saidas</p>
+            <img src={outcomeImg} alt="icone-de-saidas" />
+            </header>
+            <strong> -R$500</strong>
+        </div>
+
+        <div className="highlight-background">
+           <header>
+            <p>Total</p>
+            <img src={totalImg} alt="icone-de-entrada" />
+            </header>
+            <strong>R$500</strong>
+        </div>
+
+
+     
+
+    </Container>
+   )
 } 
