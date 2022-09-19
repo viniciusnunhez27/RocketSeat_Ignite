e70b631759f991464161f8154abfbe9191f1f438
vinicius <v.nunhez27@gmail.com>
2022-09-19 18:15:49 -0300

    Custom hook

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,5 @@
 import { FormEvent, useState, useContext } from 'react';
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/UseTransactions';
 import Modal from   'react-modal'
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
@@ -12,7 +12,7 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal ({isOpen, onRequestClose}: NewTransactionModalProps){
-   const {  createTransaction } = useContext(TransactionsContext)
+   const {  createTransaction } = useTransactions();
 
     const [title, setTitle] = useState('');
     const [amount, setAmount] = useState(0);
